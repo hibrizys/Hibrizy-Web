e6df539d23108d33e089a82817f69cb0f3b8a182
Hibrizy Sulistiyo <hibrizys@gmail.com>
2023-07-30 03:02:37 +0700

    updated img link last

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -57,8 +57,8 @@ import Contact from '@/components/Contact.vue'
 import Home from '@/components/Home.vue'
 import News from '@/components/News.vue'
 import Portofolio from '@/components/Portofolio.vue'
-import { dataImage, imgToSVG } from '../utilits'
-import { waves } from '../waves'
+import { dataImage, imgToSVG } from '../utilits.js'
+import { waves } from '../waves.js'
 
 export default {
   name: `HomeView`,
