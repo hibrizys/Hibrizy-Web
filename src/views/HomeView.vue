361fc2f9e2bb7fb4b8a88c255ef5ec1a50ed37b2
hibrizys <hibrizys@gmail.com>
2024-02-04 18:28:53 +0700

    update bug

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -49,16 +49,16 @@ import Preloader from '@/components/layout/Preloader.vue'
 import AboutPopup from '@/components/popup/AboutPopup.vue'
 import HibrizyPopup from '@/components/popup/HibrizyPopup.vue'
 import MfpPopup from '@/components/popup/MfpPopup.vue'
-import Cursor from '@/components/layout/Cursor.vue'
-import HeaderC from '@/components/layout/Header.vue'
-import Settings from '@/components/layout/Settings.vue'
-import About from '@/components/About.vue'
-import Contact from '@/components/Contact.vue'
-import Home from '@/components/Home.vue'
-import News from '@/components/News.vue'
-import Portofolio from '@/components/Portofolio.vue'
-import { dataImage, imgToSVG } from '@/utilits'
-import { waves } from "@/waves"
+import Cursor from '../components/layout/Cursor.vue'
+import HeaderC from '../components/layout/Header.vue'
+import Settings from '../components/layout/Settings.vue'
+import About from '../components/About.vue'
+import Contact from '../components/Contact.vue'
+import Home from '../components/Home.vue'
+import News from '../components/News.vue'
+import Portofolio from '../components/Portofolio.vue'
+import { dataImage, imgToSVG } from '../utilits'
+import { waves } from "../waves"
 
 export default {
   name: `HomeView`,
